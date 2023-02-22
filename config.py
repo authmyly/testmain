382e65ac4ee0a034a21bd0dc4a8af4aab1a267d8
authmyly <123830846+authmyly@users.noreply.github.com>
2023-02-22 16:03:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,36 +1,32 @@
-from os import getenv
-from dotenv import load_dotenv
+import telebot
+from googletrans import Translator
+from gtts import gTTS
+import os
 
-admins = {}
-load_dotenv()
+# Replace the API_TOKEN with your own Telegram bot token
+bot = telebot.TeleBot('6049623024:AAHNCkJ85dlPPIQFLf9uYvbhVwiV67MkwAM')
 
-# client vars
-API_ID = int(getenv("API_ID", "7452578")) #لا تغير هاذة القيمة
-API_HASH = getenv("API_HASH", "061d67ee8eed9368c5cadabb4aa21efc")#لا تغير هاذة القيمة
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-SESSION_NAME = getenv("SESSION_NAME", "")
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "أكتب أي شيء بالعربية، وانتظر قليلاً")
 
-# mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME", "rr8r9") # @ هنا ضع يوزر حسابك بدون 
-ALIVE_NAME = getenv("ALIVE_NAME", "sonng") # هنا ضع اسم حسابك
-BOT_USERNAME = getenv("BOT_USERNAME", "") # @ هنا ضع يوزر البوت بدون 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/X02lx/RrRRR") 
-UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main") #لا تغير هاذة القيمة
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60")) #لا تغير هاذة القيمة
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "rr8r9") # @ هنا ضغ يوزر كروبك بدون 
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "xl444") # @ هنا ضغ يوزر قناتك بدون
+@bot.message_handler(func=lambda message: True)
+def translate_message(message):
+    if message.text:
+        translator = Translator()
+        text_to_translate = message.text
+        translated_text = translator.translate(text_to_translate, src='ar', dest='ja').text
+        
+        # Generate an audio file for the translated text
+        tts = gTTS(text=translated_text, lang='ja')
+        tts.save('audio.mp3')
+        
+        # Send the audio file as a voice message to the user along with the translated text
+        audio_file = open('audio.mp3', 'rb')
+        bot.send_voice(message.chat.id, audio_file, caption=translated_text)
+        audio_file.close()
+        
+        # Delete the audio file from disk
+        os.remove('audio.mp3')
 
-# database, decorators, handlers mandatory vars
-MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! . $").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1854384004").split()))
-                                             #هنا ضع ايدي المطور فوق و الاعلئ
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
-
-# image resources vars
-IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
-IMG_2 = getenv("IMG_2", "https://te.legra.ph/file/90e3b3aeb77e3e598d66d.png")
-IMG_3 = getenv("IMG_3", "https://te.legra.ph/file/d70bb6fa92728763c671f.png")
-IMG_4 = getenv("IMG_4", "https://te.legra.ph/file/430dcf25456f2bb38109f.png")
-IMG_5 = getenv("IMG_5", "https://te.legra.ph/file/cd5c96a3c7e8ae1913ef3.png")
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+bot.polling()
